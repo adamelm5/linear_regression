2ea314b18900570bdf889afff05ab2c036ddbcc7
EL MOUEDDEN Adam <aelmouedden@enseirb-matmeca.fr>
2025-10-30 01:08:49 +0100

    change general case test

--- general_case/test_general_case.py
+++ general_case/test_general_case.py
@@ -1,16 +1,11 @@
 from general_case import linear_regression
 import numpy as np
-import sys
-import os
 import matplotlib.pyplot as plt
 
-
-# === Exemple d'utilisation ===
 if __name__ == "__main__":
 
-    print("\n\n##############    general case test   #############\n")
+    print("\n\n##############    General Case Test   #############\n\n")
     
-    # exemple de données où Y = X @ B
     X = np.array([
         [1, 1, 2],
         [1, 2, 3],
@@ -19,30 +14,22 @@ if __name__ == "__main__":
     ])
     b = np.array([1, 2, 3])
     Y = X @ b
-    print(f"-> Exemple where Y = X @ B :\n")
-    print(f"    X = \n{X} \n    Y = \n{Y.reshape(4, 1)}\n")
+    print("-> Y = X @ B :")
+    print(f"X =\n{X}\nY =\n{Y.reshape(-1,1)}\n")
     beta_hat, R2 = linear_regression(X, Y)
-    beta_hat = beta_hat.reshape(3, 1)
-    print(f"beta_hat = \n{beta_hat} \nand R2 = {R2}\n\n")
+    print(f"beta_hat =\n{beta_hat}\nR2 = {R2}\n\n")
 
-    # exemple de données où Y != X @ B
-    X = np.array([
-        [1, 1], [1, 2], [1, 3], [1, 4], [1, 5],
-        [1, 6], [1, 7], [1, 8], [1, 9], [1, 10]
-    ])
+    X = np.array([[1, i] for i in range(1, 11)])
     Y = np.array([8, 3, 6, 2, 7, 1, 9, 4, 5, 2])
-    print(f"-> Exemple where Y != X @ B :\n")
-    print(f"    X = \n{X} \n    Y = \n{Y}\n")
-
+    print("-> Y != X @ B :")
+    print(f"X =\n{X}\nY = {Y}\n")
     beta_hat, R2 = linear_regression(X, Y)
-    beta_hat = beta_hat
-    print(f"beta_hat = \n{beta_hat} \nand R2 = {R2}")
+    print(f"beta_hat =\n{beta_hat}\nR2 = {R2}\n\n")
 
-    # cas de dimension 1
-    print("\n\n-> One dimension case :")
-    x = [[1], [2], [3], [4], [5]]
-    y = [2, 4, 6, 8, 10]
-    print(f"    X = \n{X} \n    Y = \n{Y}\n")
-    x, y = np.array(x), np.array(y)
+
+    x = np.array([[1], [2], [3], [4], [5]])
+    y = np.array([2, 4, 6, 8, 10])
+    print("-> 1 dimension :")
+    print(f"x =\n{x}\ny = {y}\n")
     beta_hat, R2 = linear_regression(x, y)
-    print(f"beta_hat = {beta_hat} and R2 = {R2}")
+    print(f"beta_hat = {beta_hat.ravel()}\nR2 = {R2}\n\n")
